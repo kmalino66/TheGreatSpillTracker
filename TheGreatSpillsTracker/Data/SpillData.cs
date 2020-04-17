8ef67d928d128136e3d9c79d24191aeb82ae14c5
Kyle Malinowski <kmalino66@gmail.com>
2020-04-17 18:54:14 -0500

    Initial Commit

---
+++ TheGreatSpillsTracker/Data/SpillData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TheGreatSpillsTracker.Data
+{
+    [Serializable]
+    public class SpillData
+    {
+        public DateTime EnterpriseSpill { get; set; }
+        public DateTime HomeSpill { get; set; }
+
+        public int SpillCount { get; set; }
+        public int EnterpriseSpillCount { get; set; }
+        public int HomeSpillCount { get; set; }
+
+        public string EnterpriseSpillString()
+        {
+            return EnterpriseSpill.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+
+        public String HomeSpillString()
+        {
+            return HomeSpill.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
---
+++ TheGreatSpillsTracker/Data/SpillDataService.cs
@@ -0,0 +1,77 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+
+namespace TheGreatSpillsTracker.Data
+{
+    public class SpillDataService
+    {
+        private SpillData spill = new SpillData();
+
+        public SpillDataService()
+        {
+            SpillData tmp = ReadInfo();
+
+            if (tmp != null)
+            {
+                spill = tmp;
+            }
+            else
+            {
+                spill.EnterpriseSpill = DateTime.Now;
+                spill.EnterpriseSpillCount = 0;
+                spill.HomeSpill = DateTime.Now;
+                spill.HomeSpillCount = 0;
+                spill.SpillCount = 0;
+                SaveInfo();
+            }
+        }
+
+        public SpillData GetSpill()
+        {
+            return spill;
+        }
+
+        public void AddHomeSpill()
+        {
+            spill.HomeSpill = DateTime.Now;
+            spill.HomeSpillCount++;
+            spill.SpillCount++;
+        }
+
+        public void AddEnterpriseSpill()
+        {
+            spill.EnterpriseSpill = DateTime.Now;
+            spill.EnterpriseSpillCount++;
+            spill.SpillCount++;
+        }
+
+        public void SaveInfo()
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, "SpillData.json");
+            string json = JsonConvert.SerializeObject(spill, Formatting.Indented);
+            using (StreamWriter outputFile = new StreamWriter(path))
+            {
+                outputFile.WriteLine(json);
+            }
+        }
+
+        public SpillData ReadInfo()
+        {
+            string path = Path.Combine(Environment.CurrentDirectory, "SpillData.json");
+
+            if (!System.IO.File.Exists(path))
+            {
+                return null;
+            }
+
+            string json = System.IO.File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<SpillData>(json);
+        }
+    }
+}
